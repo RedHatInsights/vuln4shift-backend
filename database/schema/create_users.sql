e70fd75d2c428b8befe30113968f734011c790bc
Andrii But <abut@redhat.com>
2022-02-17 14:01:12 +0100

    add initial db schema and users

---
+++ database/schema/create_users.sql
@@ -0,0 +1,11 @@
+-- user for archive writing
+CREATE USER archive_db_writer;
+
+-- user for pyxis gathering
+CREATE USER pyxis_gatherer;
+
+-- user for VMaaS CVE sync
+CREATE USER vmaas_gatherer;
+
+-- user for CVE aggregating
+CREATE USER cve_aggregator;
