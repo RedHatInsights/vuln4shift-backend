afae8057d8cf4e8053f31fadb3849f168933d230
Tomáš Sasák <tsasak@redhat.com>
2022-03-04 15:53:30 +0100

    start using swagger in manager

--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	github.com/jackc/pgx/v4 v4.10.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.4.1
+	github.com/swaggo/swag v1.8.0 // indirect
 )
--- manager/manager.go
+++ manager/manager.go
@@ -5,7 +5,11 @@ import (
 	"app/manager/middlewares"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var openAPILocation = "/api/vuln4shift/v1/openapi.json"
 
 // createMetaGroup adds meta endpoints to the router.
 func createMetaGroup(router *gin.Engine) *gin.RouterGroup {
@@ -13,6 +17,10 @@ func createMetaGroup(router *gin.Engine) *gin.RouterGroup {
 
 	metaGroup.GET("healthz", meta.GetApistatus)
 	metaGroup.GET("apistatus", meta.GetApistatus)
+
+	openAPIURL := ginSwagger.URL(openAPILocation)
+	metaGroup.GET("openapi/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, openAPIURL))
+	metaGroup.StaticFile(openAPILocation, "./manager/docs/swagger.json")
 	return metaGroup
 }
 
@@ -31,7 +39,18 @@ func BuildRouter() *gin.Engine {
 	return router
 }
 
-// Start starts manager router.
+// @title Vulnerability for Openshift API documentation
+// @version 0.1.0
+// @description Documentation to the REST API for application
+// @description Vulnerability for Openshift based on console.redhat.com.
+//
+// @securityDefinitions.apikey RhIdentity
+// @in header
+// @name x-rh-identity
+//
+// @query.collection.format multi
+// @basepath /
+// @schemes http
 func Start() {
 	router := BuildRouter()
 	err := router.Run(":8000")
