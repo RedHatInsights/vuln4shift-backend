17005247a3438c0ffb0e3814dc2944ce2fb12508
Tomáš Sasák <tsasak@redhat.com>
2022-03-04 15:53:30 +0100

    start using swagger in manager

--- manager/controllers/meta/apistatus.go
+++ manager/controllers/meta/apistatus.go
@@ -3,6 +3,14 @@ package meta
 import "github.com/gin-gonic/gin"
 
 // GetApistatus represents health/status endpoint controller.
+//
+// @id GetApistatus
+// @summary API status of the application
+// @description Endpoint checks for status of the application
+// @accept */*
+// @router /apistatus [get]
+// @success 200
+// @failure 503
 func GetApistatus(ctx *gin.Context) {
 	ctx.Status(200)
 }
