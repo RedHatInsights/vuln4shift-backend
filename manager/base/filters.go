23e607da8032375b350ff8875a5762b39617b3b0
Tomáš Sasák <tsasak@redhat.com>
2022-03-31 20:40:46 +0200

    add filters and their parsers

---
+++ manager/base/filters.go
@@ -0,0 +1,252 @@
+package base
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"app/base/models"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+const DateFormat = "2006-01-02"
+const (
+	SearchQuery           = "search"
+	PublishedQuery        = "published"
+	SeverityQuery         = "severity"
+	CvssScoreQuery        = "cvss_score"
+	AffectedClustersQuery = "affected_clusters"
+	AffectedImagesQuery   = "affected_images"
+	LimitQuery            = "limit"
+	OffsetQuery           = "offset"
+	SortQuery             = "sort"
+)
+
+const (
+	SortFilterArgs = "sort_filter"
+)
+
+// Filter interface, represents filter obtained from
+// query argument in request link.
+type Filter interface {
+	ApplyQuery(*gorm.DB, map[string]interface{}) error
+	RawQueryVal() string
+	RawQueryVals() []string
+	RawQueryName() string
+}
+
+// RawFilter implements Filter interface, contains
+// raw name of query argument and raw parsed query
+// values in string
+type RawFilter struct {
+	RawParam  string
+	RawValues []string
+}
+
+// RawParamName getter to the parameter name in query
+func (b *RawFilter) RawQueryName() string {
+	return b.RawParam
+}
+
+// RawQueryVal returns obtained raw values formatted in query value string
+func (b *RawFilter) RawQueryVal() string {
+	return strings.Join(b.RawValues[:], ",")
+}
+
+// RawQueryVals returns parsed raw values from query value string
+func (b *RawFilter) RawQueryVals() []string {
+	return b.RawValues
+}
+
+// CveSearch represents filter for CVE substring search
+// ex. search=CVE-2022
+type CveSearch struct {
+	RawFilter
+	value string
+}
+
+// ApplyQuery filters CVEs by their substring match name or description
+func (c *CveSearch) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	regex := fmt.Sprintf("%%%s%%", c.value)
+	tx.Where("cve.name LIKE ? OR cve.description LIKE ?", regex, regex)
+	return nil
+}
+
+// CvePublishDate represents filter for CVE publish date filtering
+// ex: publsihed=2021-01-01,2022-02-02
+type CvePublishDate struct {
+	RawFilter
+	From time.Time
+	To   time.Time
+}
+
+// ApplyQuery filters CVEs by their public date limit
+func (c *CvePublishDate) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	tx.Where("cve.public_date >= ? AND cve.public_date <= ?", c.From, c.To)
+	return nil
+}
+
+// Severity represents CVE severity filter
+// ex. severity=critical,important,none
+type Severity struct {
+	RawFilter
+	Value []models.Severity
+}
+
+// ApplyQuery filters CVEs by their severity
+func (s *Severity) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	tx.Where("cve.severity IN ?", s.Value)
+	return nil
+}
+
+// CvssScore represents filter for CVE cvss2/3 score range
+// cvss_score=0.0,9.0
+type CvssScore struct {
+	RawFilter
+	From float32
+	To   float32
+}
+
+// ApplyQuery filters CVEs by cvss2/3 score range
+func (c *CvssScore) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	tx.Where("COALESCE(cve.cvss3_score, cve.cvss2_score) >= ? AND COALESCE(cve.cvss3_score, cve.cvss2_score) <= ?", c.From, c.To)
+	return nil
+}
+
+// To be implemented
+type AffectingClusters struct {
+	RawFilter
+	OneOrMore bool
+	None      bool
+}
+
+func (a *AffectingClusters) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	return nil
+}
+
+// To be implemented
+type AffectingImages struct {
+	RawFilter
+	OneOrMore bool
+	None      bool
+}
+
+func (a *AffectingImages) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	return nil
+}
+
+// Limit filter sets number of data objects per page
+// ex. limit=20
+type Limit struct {
+	RawFilter
+	Value uint64
+}
+
+// ApplyQuery limits the number of data in query - limit per page
+func (l *Limit) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	tx.Limit(int(l.Value))
+	return nil
+}
+
+// Offset filter sets an offset of data in query - start of the page
+// ex. offset=40
+type Offset struct {
+	RawFilter
+	Value uint64
+}
+
+// ApplyQuery sets and offset from the rows result
+func (o *Offset) ApplyQuery(tx *gorm.DB, _ map[string]interface{}) error {
+	tx.Offset(int(o.Value))
+	return nil
+}
+
+// SortItem represents an single column row sort expression
+// Used by the Sort filter
+type SortItem struct {
+	Column string
+	Desc   bool
+}
+
+// SortArgs represents an argument for Sort filter
+// SortableColumns represents mapping from user selected column
+// to the correct sql expression column
+// DefaultSortable contains an default sorting defined by controller
+type SortArgs struct {
+	SortableColums  map[string]string
+	DefaultSortable []SortItem
+}
+
+// Sort filter sorts an query by given list of sort item expressions
+// ex. sort=synopsis,cvss_score
+type Sort struct {
+	RawFilter
+	Values []SortItem
+}
+
+// ApplyQuery sorts the resulting query, query is sorted
+// 1st - by user defined columns
+// 2nd - by controller selected default columns
+func (s *Sort) ApplyQuery(tx *gorm.DB, args map[string]interface{}) error {
+	if i, exists := args[SortFilterArgs]; exists {
+		sortArgs, ok := i.(SortArgs)
+		if !ok {
+			return nil
+		}
+		// Sort by user selected columns
+		for _, item := range s.Values {
+			// Check if selected user column is mappable to sortable column sql expression
+			if col, exists := sortArgs.SortableColums[item.Column]; exists {
+				if item.Desc {
+					tx.Order(fmt.Sprintf("%s DESC NULLS LAST", col))
+				} else {
+					tx.Order(fmt.Sprintf("%s ASC NULLS LAST", col))
+				}
+			} else {
+				return errors.New("Invalid sort column selected")
+			}
+		}
+		// Sort by default sortable
+		for _, item := range sortArgs.DefaultSortable {
+			if col, exists := sortArgs.SortableColums[item.Column]; exists {
+				// Always add the default sort parameter, so user can see default sort
+				if item.Desc {
+					tx.Order(fmt.Sprintf("%s DESC NULLS LAST", col))
+					s.RawValues = append(s.RawValues, fmt.Sprintf("-%s", item.Column))
+				} else {
+					tx.Order(fmt.Sprintf("%s ASC NULLS LAST", col))
+					s.RawValues = append(s.RawValues, item.Column)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// GetRequestedFilters gets requested parsed filters from gin context
+// returns empty map if not exists
+func GetRequestedFilters(ctx *gin.Context) map[string]Filter {
+	if f, exists := ctx.Get("filters"); exists {
+		if f, ok := f.(map[string]Filter); ok {
+			return f
+		}
+	}
+	return map[string]Filter{}
+}
+
+// ApplyFilters, applies requested filters from query params on created query from controller,
+// filters needs to be allowed from controller in allowedFilters array
+func ApplyFilters(query *gorm.DB, allowedFilters []string, requestedFilters map[string]Filter, args map[string]interface{}) error {
+	for _, allowedFilter := range allowedFilters {
+		if filter, requested := requestedFilters[allowedFilter]; requested {
+			err := filter.ApplyQuery(query, args)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
---
+++ manager/middlewares/filterer.go
@@ -0,0 +1,63 @@
+package middlewares
+
+import (
+	"app/manager/base"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Golang HTTP module does not parse query
+// array arguments such as -> filter=1,2,3&filter=4 -> ["1,2,3", "4"]
+// which should be ["1", "2", "3", "4"]
+func ParseCommaParams(values []string) []string {
+	var res []string
+	for _, val := range values {
+		vals := strings.Split(val, ",")
+		res = append(res, vals...)
+	}
+	return res
+}
+
+func ApplyDefaultFilters(requestedFilters map[string]base.Filter) {
+	// set default limit paging if not set
+	if _, exists := requestedFilters[base.LimitQuery]; !exists {
+		requestedFilters[base.LimitQuery] = &base.Limit{
+			RawFilter: base.RawFilter{RawParam: "limit", RawValues: []string{"20"}},
+			Value:     uint64(20)}
+	}
+	// set default offset paging if not set
+	if _, exists := requestedFilters[base.OffsetQuery]; !exists {
+		requestedFilters[base.OffsetQuery] = &base.Offset{
+			RawFilter: base.RawFilter{RawParam: "offset", RawValues: []string{"0"}},
+			Value:     uint64(0)}
+	}
+	// always sort, at least default values
+	if _, exists := requestedFilters[base.SortQuery]; !exists {
+		requestedFilters[base.SortQuery] = &base.Sort{
+			RawFilter: base.RawFilter{RawParam: "sort", RawValues: []string{}},
+			Values:    []base.SortItem{},
+		}
+	}
+}
+
+func Filterer() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		filters := make(map[string]base.Filter)
+		for param, rawValues := range ctx.Request.URL.Query() {
+			values := ParseCommaParams(rawValues)
+			filter, err := base.ParseFilter(param, values)
+			if err != nil && errors.Is(err, base.ErrInvalidFilterArgument) {
+				continue
+			} else if err != nil {
+				ctx.AbortWithStatusJSON(http.StatusBadRequest, base.BuildErrorResponse(http.StatusBadRequest, err.Error()))
+				return
+			}
+			filters[filter.RawQueryName()] = filter
+		}
+		ApplyDefaultFilters(filters)
+		ctx.Set("filters", filters)
+	}
+}
