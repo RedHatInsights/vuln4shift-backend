1a0932c5041af00313d3c8b412c928f3a258a9a5
Jan Dobes <jdobes@redhat.com>
2022-05-09 13:54:58 +0200

    apistatus shouldn't be in apidoc, it's not reachable from outside

--- manager/controllers/meta/apistatus.go
+++ manager/controllers/meta/apistatus.go
@@ -2,15 +2,6 @@ package meta
 
 import "github.com/gin-gonic/gin"
 
-// GetApistatus represents health/status endpoint controller.
-//
-// @id GetApistatus
-// @summary API status of the application
-// @description Endpoint checks for status of the application
-// @accept */*
-// @router /apistatus [get]
-// @success 200
-// @failure 503
 func (e *Controller) GetApistatus(ctx *gin.Context) {
 	ctx.Status(200)
 }
