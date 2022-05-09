05d31d3f4fdd18a81f3c872b4b8f1c29f2b311a5
Jan Dobes <jdobes@redhat.com>
2022-05-09 14:13:37 +0200

    disable the swagger 3.0 conversion, it doesn't convert references correctly

--- scripts/generate_swagger.sh
+++ scripts/generate_swagger.sh
@@ -3,19 +3,15 @@
 set -e
 
 CONVERT_URL="https://converter.swagger.io/api/convert"
-DOCS_TMP="/tmp"
+DOCS_DIR="./manager/docs"
 
 if ! which "$GOPATH"/bin/swag &> /dev/null; then
     go install github.com/swaggo/swag/cmd/swag@latest
 fi
 
-# Generate 2.0 swagger
-"$GOPATH"/bin/swag init -g ./manager/manager.go --output $DOCS_TMP
-
-if ! ls ./manager/docs &> /dev/null; then
-  mkdir ./manager/docs
+if ! ls "$DOCS_DIR" &> /dev/null; then
+  mkdir "$DOCS_DIR"
 fi
 
-# Convert 2.0 -> 3.0
-curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
-     -d @$DOCS_TMP/swagger.json $CONVERT_URL > ./manager/docs/swagger.json
+# Generate 2.0 swagger
+"$GOPATH"/bin/swag init -g ./manager/manager.go --output $DOCS_DIR
