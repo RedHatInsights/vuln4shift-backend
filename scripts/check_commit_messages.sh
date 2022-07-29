3554d1b1d50313cf22c0ad8457a6623a784fb500
Jan Dobes <jdobes@redhat.com>
2022-07-29 15:41:40 +0200

    chore(semantic-release): check commit messages
    second line

---
+++ scripts/check_commit_messages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+commitTitle="$1"
+
+# ignore merge requests
+if echo "$commitTitle" | grep -qE "^Merge branch \'"; then
+	echo "Commit hook: ignoring branch merge"
+	exit 0
+fi
+
+# check semantic versioning scheme
+if ! echo "$commitTitle" | grep -qE '^(feat|fix|docs|style|refactor|perf|test|chore)\(?.*\)?:\s\w+'; then
+	echo "Your commit title did not follow semantic versioning: $commitTitle"
+	echo "Please see https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commit-message-format"
+	exit 1
+fi
