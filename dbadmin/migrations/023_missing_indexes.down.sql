5455869ad1b3fc2ddfb577ffe00938f1c32758e3
Jan Dobes <jdobes@redhat.com>
2024-04-29 14:48:09 +0200

    fix: add indexes to improve perf when selecting from CVE and image side

---
+++ dbadmin/migrations/023_missing_indexes.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX image_cve_cve_id_idx;
+DROP INDEX cluster_image_image_id_idx;
+DROP INDEX repository_image_image_id_idx;
---
+++ dbadmin/migrations/023_missing_indexes.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX ON image_cve(cve_id);
+CREATE INDEX ON cluster_image(image_id);
+CREATE INDEX ON repository_image(image_id);
